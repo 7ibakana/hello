500b74326eb99edfdeab93afdb730d7817591ae8
yu2519hn <1234568hH*>
2020-09-17 02:06:18 -0500

    classes taking

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 classesTaken = input("Please enter the nanumber of all the ITEC classes you are taking this semester seperated by a space: ")
 print("\n")
-print("You are taking the following ITEC courses")
+print("You are taking the following ITEC courses:")
 for classes in classesTaken.split():
     print(f"ITEC{classes}")
